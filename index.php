75c76e9260d2d62c40933eb1bab873a6c7dbf4f4
bry <M303User@m303.uc-bcf.edu.ph>
2025-03-06 16:31:34 +0800

    Exercise 1

---
+++ index.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        $a = 15;
+        $b = 7;
+        
+        $sum = $a + $b;
+        $difference = $a - $b;
+        $product = $a * $b;
+        $quotient = $a / $b;
+        
+        echo 'The sum is: '.$sum ."\n";
+        echo 'The difference is: '.$difference ."\n";
+        echo 'The product is: '.$product ."\n";
+        echo 'The quotient is: '.$quotient;
+    ?>
+</body>
+</html>
